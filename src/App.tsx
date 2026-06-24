612415ba1735238e271b23cabed8da16ac134314
pol-pil <polsapol555@gmail.com>
2026-06-24 18:16:47 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from './components/theme-provider'
 function App() {
    return (
       <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-         <div className='absolute z-10 p-4 text-foreground'>
+         <div className='absolute z-100 p-2 lg:p-4 text-foreground'>
             <ModeToggle />
          </div>
          <div className='h-dvh w-full p-4 sm:px-8 lg:px-24 xl:px-40'>
