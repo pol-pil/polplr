7db3e824be180184736e8f63eba087c52377bf8a
pol-pil <polsapol555@gmail.com>
2026-07-02 13:40:02 +0800

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import ImageTrail from './components/image-trail'
 import { ResizableMain } from './components/resizable-main'
 import { ThemeProvider } from './components/theme-provider'
-
-
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
    return (
       <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+         <Analytics />
          <div className='h-dvh w-full p-4 sm:px-8 lg:px-24 xl:px-40'>
             {/* ImageTrail behind everything */}
             <div className='fixed inset-0 z-0'>
